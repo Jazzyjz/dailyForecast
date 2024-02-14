688da26bfb0c50e5733df7ab4d287fd8013db108
jazzyjz <jasminf2202@gmail.com>
2024-02-14 00:29:53 -0500

    added begginer javascript, added bg image, added more style to some card elements

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,16 @@
-var a = "10" ;
-var b = "15" ;
+// variables
 
 
-console.log(a === b);//type and value
 
-console.log(a==b);
+// event listeners
 
+// API key
+var apiKey = "932891f970d879716f109a3b5af52d42"
+
+// // current day api 
+// var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="{city name}"&appid="+apiKey;
+
+// rest of week api
+var weeklyWeather = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid="+ apiKey;
+
+// get current day forecast
