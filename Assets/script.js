8ce5efd992d92cde7ace4c458fb37381c544375e
jazzyjz <jasminf2202@gmail.com>
2024-02-15 15:19:50 -0500

    added global and element variables to js workingon functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,36 @@
-// variables
+// Global variables
+var city;
 
+// element variables
 
+var searchBtn = document.querySelector('#searchbtn');
+var cityInputEl = document.querySelector('#cityInput');
 
 // event listeners
+searchBtn.addEventListener('click', submitForm);
 
 // API key
 var apiKey = "932891f970d879716f109a3b5af52d42"
 
 // // current day api 
-var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="+ "&appid="+apiKey;
+var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="+ +"&appid="+apiKey;
 
 // rest of week api
-var weeklyWeather = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid="+ apiKey;
+var weeklyWeather = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q="+ +"&appid="+ apiKey;
+
+
+// function to submit city input
+async function submitForm(){
+    if(!cityInputEl.value){
+        alert("Enter a city name ");
+        return;
+    }
+    var search = cityInputEl.value;
+    
+}
 
 // get current day forecast
+
 async function getdayForecast() {
     fetch(currentWeather)
     const response = await fetch(currentWeather);
@@ -27,23 +44,29 @@ async function getWeekForecast() {
     return data;
 }
 
-async function dayOfForecast() {
-    var today = await getdayForecast();
-    // create var for new element of day
+async function getForecast(){
 
-    // inject data in var
-
-    // append var data in created element
+    async function dayOfForecast() {
+        var today = await getdayForecast();
+        // create var for new element of day
+    
+        // inject data in var
+    
+        // append var data in created element
+    }
+    
+        // get rest of week forecast
+    async function weekForecast() {
+        var week = await getWeekForecast();
+        // create for loop for each card to be created with data limits
+    
+         // create var for new element of day
+    
+        // inject data in var
+    
+        // append var data in created element
+    }
+    dayOfForecast()
+    weekForecast()
 }
 
-    // get rest of week forecast
-async function weekForecast() {
-    var week = await getWeekForecast();
-    // create for loop for each card to be created with data limits
-
-     // create var for new element of day
-
-    // inject data in var
-
-    // append var data in created element
-}
